a0c5e5fcec20c0bfbf814e1333537b130bd17a4d
wushichatong <1017672410@qq.com>
2015-02-26 14:27:57 +0800

    change it

--- sky.js
+++ sky.js
@@ -1,10 +1,13 @@
 /**
  * Created by JamesMartins on 2015/2/24.
  */
-var angle=0;
 window.onload=function(){
     var canvas=document.getElementById("canvas");
     var context=canvas.getContext("2d");
+    var canvas_wrapper=document.getElementById("canvas-wrapper");
+
+
+
 
     var winWidth;
     var winHeight;
@@ -24,10 +27,13 @@ window.onload=function(){
     winHeight = document.documentElement.clientHeight;
     winWidth = document.documentElement.clientWidth;
     }
-    var WINDOW_HEIGHT=winHeight;
-    var WINDOW_WIDTH=winWidth;
-    canvas.width=WINDOW_WIDTH;
-    canvas.height=WINDOW_HEIGHT;
+
+    canvas.width=winWidth;
+    canvas.height=winHeight;
+
+    //set the canvas-wrapper sie
+    canvas_wrapper.style.width=canvas.width;
+    canvas_wrapper.style.height=canvas.height;
 
     //background color
     var skyStyle=context.createRadialGradient(
@@ -60,31 +66,13 @@ window.onload=function(){
             context.beginPath();
             context.globalAlpha = 1;
 
-            //context.rotate(angle*Math.PI/180);
             context.scale(Math.sin(angle * Math.PI / 180), Math.sin(angle * Math.PI / 180));
-//                    if (i == Math.random() * 200) {
-//                        for (var i = 0; i < 360; i++) {
-//
-////                            context.globalAlpha = Math.abs(Math.sin(i / 180 * Math.PI / 1000));
-//                            context.save();
-////                            context.scale(10*Math.sin(angle * Math.PI / 180), 10*Math.sin(angle * Math.PI / 180));
-//                            drawStar(context, star[i].x, star[i].y, star[i].r, star[i].r / 2.0, star[i].a);
-//                            context.restore();
-//                        }
-//
-//                    } else drawStar(context, star[i].x, star[i].y, star[i].r, star[i].r / 2.0, star[i].a);
-//                context.translate(x,y);
             drawStar(context, star[i].x, star[i].y, star[i].r, star[i].r / 2.0, star[i].a);
             context.restore();
-
         }
-        //context.restore();
         context.fillStyle = skyStyle;
-        //context.fillRect(0,0,canvas.width,canvas.height);
         drawLand(canvas,context);
     },400);
-    //setInterval(update(canvas,context),60);
-
     };
 
 function drawStar(context,x,y,outerR,innerR,rot){
@@ -107,34 +95,15 @@ function drawStar(context,x,y,outerR,innerR,rot){
     context.restore();
 
     }
-//    function drawStar(context,outerR,innerR,rot){
-//        context.save();
-//        context.beginPath();
-//        for (var i=0;i<5;i++){
-//            context.lineTo(Math.cos((18+72*i-rot)/180*Math.PI)*outerR,
-//                            -Math.sin((18+72*i-rot)/180*Math.PI)*outerR);
-//            context.lineTo(Math.cos((54+72*i-rot)/180*Math.PI)*innerR,
-//                            -Math.sin((54+72*i-rot)/180*Math.PI)*innerR);
-//
-//        }
-//        context.closePath();
-//        context.fillStyle="#fb3";
-//        context.lineJoin="round";
-//        context.strokeStyle="#fd5";
-//        context.lineWidth=3;
-//        context.fill();
-//        context.stroke();
-//        context.restore();
-//
-//    }
+
 function drawLand(canvas,cxt){
     cxt.save();
 
     cxt.beginPath();
-    cxt.moveTo(0,0.75*canvas.height)
+    cxt.moveTo(0,0.75*canvas.height);
     cxt.bezierCurveTo(0.45*canvas.width,0.6*canvas.height,
     0.6*canvas.width,0.8*canvas.height,
-    canvas.width,0.65*canvas.height)
+    canvas.width,0.65*canvas.height);
     cxt.lineTo(canvas.width,canvas.height);
     cxt.lineTo(0,canvas.height);
     cxt.closePath();
